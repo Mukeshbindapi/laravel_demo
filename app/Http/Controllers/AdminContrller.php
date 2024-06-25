8ef8ad7d4fa6fbe2af7ff1568769355d72661d98
Mukeshbindapi <112742726+Mukeshbindapi@users.noreply.github.com>
2024-06-25 09:58:07 +0530

    first commit

---
+++ app/Http/Controllers/AdminContrller.php
@@ -0,0 +1,64 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
+
+class AdminContrller extends Controller
+{
+    public function create(){
+        // echo "Admin Create";
+        // dd('store admin dd');
+        // dump('store admin');
+
+        $data = [
+            'first_name' => 'lakshimina',
+            'last_name' => 'rajbhar',
+            'email' => 'lakshiminaraj@gmail.com',
+            'gender' => 'female',
+            'city_id' => 4,
+            'address' => 'manikpur',
+            'mobile_no' => 3579742805,
+            'type' => 1,
+            'is_active' => 1,
+        ];
+
+        $admin = DB::table('admins')->insert($data);
+        dd('admin created successfully.',$admin,$data);     
+    }
+
+    public function list(){
+        $admins = DB::table('admins')->get();
+        // dd($admins[3]);
+        foreach($admins as $admin){
+            dump($admin);
+        }
+    }
+
+    public function update(){
+        $id = 4;
+        $data = [
+            'first_name' => 'lakshimina',
+            'last_name' => 'bind',
+            'email' => 'lakshiminabind@gmail.com',
+            'gender' => 'female',
+            'city_id' => 4,
+            'address' => 'manikpur',
+            'mobile_no' => 9876543765,
+            'type' => 1,
+            'is_active' => 1,
+            'created_at' => now(),
+            'updated_at' => now()
+        ];
+
+        DB::table('admins')->where('id',$id)->update($data);
+        dd('admin Updated successfully.',$data);  
+    }
+
+    public function remove(){
+        $id = 2;
+        DB::table('admins')->where('id',$id)->delete();
+        dd('admin Updated successfully.');
+    }
+}
---
+++ routes/web.php
@@ -0,0 +1,20 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\AdminContrller;
+
+// Routing Methods
+// get->view
+// post->store
+// put->store
+// patch->store || update
+// delete->Delete
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/admin/create',[AdminContrller::class,'create']);
+Route::get('/admin/list',[AdminContrller::class,'list']);
+Route::get('/admin/update',[AdminContrller::class,'update']);
+Route::get('/admin/delete',[AdminContrller::class,'remove']);
